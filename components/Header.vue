632f43735669a075c8fbef445fe8173d20961a19
Ad <thenewadeel@gmail.com>
2023-03-24 00:48:22 +0500

    login?

--- components/Header.vue
+++ components/Header.vue
@@ -11,12 +11,37 @@
       <li><nuxt-link to="/blog" @click.native="menuChecked = false">Blog</nuxt-link></li>
       <li><nuxt-link to="/contact" @click.native="menuChecked = false">Contact</nuxt-link></li>
       <li><nuxt-link to="/about" @click.native="menuChecked = false">About Us</nuxt-link></li>
+      <!-- Add a menu:
+   Log in / Sign up - when the user is not logged in
+   Username / Log out - when the user is logged in
+  -->
+      <li>
+        <div class="button-primary" data-netlify-identity-menu></div>
+      </li>
+
+      <!-- Add a simpler button:
+  Simple button that will open the modal.
+-->
+      <!-- <div data-netlify-identity-button>Login with Netlify Identity</div> -->
     </ul>
   </header>
 </template>
 
 <script>
 export default {
-  props: ['menuChecked']
+  props: ['menuChecked'],
+  mounted() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }
 }
+
+
 </script>
---
+++ content/posts/Hello World.md
@@ -0,0 +1,9 @@
+---
+title: Pilot Post
+seo_title: Pilot Post - SEO Version
+summary: First post in a series of test post
+seo_desc: First post in a series of test post - SEO Version
+date: 2023-03-23
+---
+
+### title
